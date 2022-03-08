1cdd2b1b1f5d85505072d98c174bb69f871c11b5
andrewstevenhahn <95725201+andrewstevenhahn@users.noreply.github.com>
2022-03-07 22:56:08 -0600

    Added map application and content. Fixed nav bar.

--- js/main.js
+++ js/main.js
@@ -1,54 +1,215 @@
-console.log("Hello")
+//Code adapted from https://cartographicperspectives.org/index.php/journal/article/view/cp76-donohue-et-al/1307
 
-/* Example from Leaflet Quick Start Guide*/
 $(document).ready(function () {
-    var cities;
-    var map = L.map('map').setView([43.7844, -88.7879], 6);
+    var wells;
+    var map = L.map('map').setView([44.7, -89.7], 7);
+
+    function processData(data) {
+        var timestamps = [];
+        var min = Infinity;
+        var max = -Infinity;
+
+        for (var feature in data.features) {
+            var properties = data.features[feature].properties;
+            for (var attribute in properties) {
+
+                if (['yr2010','yr2011','yr2012','yr2013','yr2014','yr2015','yr2016','yr2017','yr2018','yr2019','yr2020','yr2021'].includes(attribute)) {
+                    
+                    attribute = attribute.replace('yr','')
+                    
+                    if ( !timestamps.includes(attribute) ) {
+                        timestamps.push(attribute);
+                    }
+
+                    if (properties[attribute] < min) {
+                        min = properties[attribute];
+                    }
+
+                    if (properties[attribute] > max) {
+                        max = properties[attribute];
+                    }
+                }
+            }
+
+        }
+
+        return {
+            timestamps: timestamps,
+            min: min,
+            max: max
+        }
+    }
+
+    function calcPropRadius(attributeValue) {
+        var scaleFactor = 20;
+        var area = attributeValue * scaleFactor;
+        return Math.sqrt(area/Math.PI)*2;
+    }
+
+    function updatePropSymbols(timestamp) {
+        
+        wells.eachLayer(function(layer) {
+            var timestamp2 = "yr" + timestamp.toString();
+            var prevtime = "yr" + (timestamp-1).toString();
+            var props = layer.feature.properties;
+            var radius = calcPropRadius(props[timestamp2]);
+            var newwells = props[timestamp2] - props[prevtime];
+            console.log(String(props.timestamp2) + "-" + String(props.prevtime))
+            if (isNaN(newwells)) {
+                var newwells = 0;
+            }
+            var popupContent = "<b> " + props["TWP"] + "N " + props["RNG"] + props["DIR_ALPHA"] + "</b><br>"
+                    +"<i>" + String(props[timestamp2]) + " total high-capacity <br /> wells built between <br /> 2010 and "+ String(timestamp) +"</i>"
+            
+            layer.setRadius(radius)
+            layer.bindPopup(popupContent, { offset: new L.Point(0,-radius)});
+
+        });
+    }
+
+    function createPropSymbols(timestamps, data) {
+        wells = L.geoJson(data, {
+
+            pointToLayer: function(feature,latlng) {
+  
+                return L.circleMarker(latlng, {
+                    fillColor: "#708598",
+                    color: "#537898",
+                    weight: 1,
+                    fillOpacity: 0.6
+
+                }).on({
+                    
+                    mouseover: function(e) {
+                        this.openPopup();
+                        this.setStyle({color: "yellow"})
+
+                    },
+                    mouseout: function(e) {
+                        this.closePopup();
+                        this.setStyle({color:"#537898"})
+                    }
+
+                });
+
+            }
+
+        }).addTo(map);
+
+        updatePropSymbols(timestamps[0]);
+
+    }
+
+    function createLegend(min, max) {
+
+        min = 1
+        max = 20
+
+        function roundNumber(inNumber) {
+
+                return (Math.round(inNumber/1) * 1);  
+        }
+
+        var legend = L.control( { position: 'bottomright' } );
+
+        legend.onAdd = function(map) {
+
+        var legendContainer = L.DomUtil.create('div', 'legend');  
+        var symbolsContainer = L.DomUtil.create('div', "symbolsContainer");
+        var classes = [roundNumber(min), roundNumber((max-min)/2), roundNumber(max)]; 
+        var legendCircle;  
+        var lastRadius = 0;
+        var currentRadius;
+        var margin;
+
+        L.DomEvent.addListener(legendContainer, 'mousedown', function(e) { 
+            L.DomEvent.disableClickPropagation(this); 
+        });  
+
+        $(legendContainer).append("<h2 id='legendTitle'>New Wells Built<br>since 2010</h2>");
+        
+        for (var i = 0; i <= classes.length-1; i++) {  
+
+            legendCircle = L.DomUtil.create("div", "legendCircle");  
+            
+            currentRadius = calcPropRadius(classes[i]);
+            
+            margin = -currentRadius - lastRadius - 2;
+
+            $(legendCircle).attr("style", "width: " + currentRadius*2 + 
+                "px; height: " + currentRadius*2 + 
+                "px; margin-left: " + margin + "px" );				
+            $(legendCircle).append("<span class='legendValue'>"+classes[i]+"</span>");
+
+            $(symbolsContainer).append(legendCircle);
+
+            lastRadius = currentRadius;
+
+        }
+
+        $(legendContainer).append(symbolsContainer); 
+
+        return legendContainer; 
+
+        };
+
+        legend.addTo(map);  
+
+    } // end createLegend();
+
+    function createSliderUI(timestamps) {
+
+        var sliderControl = L.control({ position: 'bottomleft'} );
+
+        sliderControl.onAdd = function(map) {
+
+            var slider = L.DomUtil.create("input", "range-slider");
+
+            L.DomEvent.disableClickPropagation(slider);
+
+            $(slider)
+                .attr({'type':'range', 
+                    'max': timestamps[timestamps.length-1], 
+                    'min': timestamps[0], 
+                    'step': 1,
+                    'value': String(timestamps[0])})
+                .on('input change', function() {
+                updatePropSymbols($(this).val().toString());
+                    $(".temporal-legend").text(this.value);
+            });
+            return slider;
+        }
+
+        sliderControl.addTo(map)
+        createTemporalLegend(timestamps[0]) 
+    }
+
+	function createTemporalLegend(startTimestamp) {
+
+		var temporalLegend = L.control({ position: 'bottomleft' }); 
+
+		temporalLegend.onAdd = function(map) { 
+			var output = L.DomUtil.create("output", "temporal-legend");
+ 			$(output).text(startTimestamp)
+			return output; 
+		}
+
+		temporalLegend.addTo(map); 
+	}
 
     L.tileLayer(
-        'https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png', {
-            attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+        'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 12,
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             }).addTo(map);
     
-    console.log("Does this work")
+    $.getJSON("data/plss_centroids_wellconstruction.geojson")
+            .done(function(data) {
+                var info = processData(data)
+                createPropSymbols(info.timestamps, data)
+                createLegend(info.min,info.max);
+                createSliderUI(info.timestamps);
+            })
+    .fail(function() {alert("There has been a problem loading the data.")});
+
 });
-
-//add tile layer...replace project id and accessToken with your own
-// L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-//     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery   <a href="http://mapbox.com">Mapbox</a>',
-//     maxZoom: 18,
-//     id: 'username.ab12cdef',
-//     accessToken: 'pk.eyJ1IjoiYXNoYWhuIiwiYSI6ImNrd2NwcG03eWd0eXYyb3BnNWw3OG1oNnUifQ.RrIu8UiXFUike24U2SlJmg'
-// }).addTo(map);
-
-// var marker = L.marker([51.5, -0.09]).addTo(map);
-
-// var circle = L.circle([51.508, -0.11], 500, {
-//     color: 'red',
-//     fillColor: '#f03',
-//     fillOpacity: 0.5
-// }).addTo(map);
-
-// var polygon = L.polygon([
-//     [51.509, -0.08],
-//     [51.503, -0.06],
-//     [51.51, -0.047]
-// ]).addTo(map);
-
-// marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-// circle.bindPopup("I am a circle.");
-// polygon.bindPopup("I am a polygon.");
-
-// var popup = L.popup()
-//     .setLatLng([51.5, -0.09])
-//     .setContent("I am a standalone popup.")
-//     .openOn(map);
-
-// var popup = L.popup();
-
-// function onMapClick(e) {
-//     popup
-//         .setLatLng(e.latlng)
-//         .setContent("You clicked the map at " + e.latlng.toString())
-//         .openOn(map);
-// }
